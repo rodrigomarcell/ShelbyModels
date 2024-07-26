ea4a9005c943eb5e1cecdf2f5340493d12b0c811
Rodrigo Marcel <rodrigomarcell@outlook.com>
2024-07-26 19:04:52 -0400

    Migratios 20240726230401_AjustesTabelaUsuarios

--- ShelbyModels.Infra/Migrations/20240726230401_AjustesTabelaUsuarios.Designer.cs
+++ ShelbyModels.Infra/Migrations/20240726230401_AjustesTabelaUsuarios.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShelbyModels.Infra.Data;
 
@@ -11,9 +12,11 @@ using ShelbyModels.Infra.Data;
 namespace ShelbyModels.Infra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240726230401_AjustesTabelaUsuarios")]
+    partial class AjustesTabelaUsuarios
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
